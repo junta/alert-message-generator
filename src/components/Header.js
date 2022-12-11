8e46393faf9e6621259ac16fa43f62d8d8daa8c6
0xIbuki <junta8918@gmail.com>
2022-12-11 17:44:34 +0900

    modify title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { AppBar, Toolbar } from '@material-ui/core';
 const Header = () => {
   return (
     <AppBar position="static" style={{ backgroundColor: '#000000' }}>
-      <Toolbar>Alert Message Generator - Tradingview Strategy Integration</Toolbar>
+      <Toolbar>Alert Message Generator - Tradingview Alert Connector</Toolbar>
     </AppBar>
   );
 };
